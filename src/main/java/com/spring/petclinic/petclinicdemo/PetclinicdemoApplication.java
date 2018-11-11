c99d3c747f707ec5f95b4d0515d9098b1f7b983c
hiteshkataria <hitesh.kataria@idemia.com>
2018-11-11 13:25:56 +0530

    Initializing git PetClinic Repository

---
+++ src/main/java/com/spring/petclinic/petclinicdemo/PetclinicdemoApplication.java
@@ -0,0 +1,12 @@
+package com.spring.petclinic.petclinicdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclinicdemoApplication.class, args);
+	}
+}
