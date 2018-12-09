0f4be0ebf28027c45fdb857f115dcb82a6dac67a
hiteshkataria <kataria.hitesh@yahoo.in>
2018-12-09 23:47:12 +0530

    Showing list of owners on the UI. Close #8

--- pet-clinic-web/src/main/java/com.spring.petclinic.petclinicdemo/Controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com.spring.petclinic.petclinicdemo/Controllers/OwnerController.java
@@ -1,15 +1,25 @@
 package com.spring.petclinic.petclinicdemo.Controllers;
 
+import com.spring.petclinic.petclinicdemo.service.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"/owners","/owners.html"})
 @Controller
 public class OwnerController {
 
-    @RequestMapping({})
-    public String listOwners(){
-        return "owners/index";
+    private final OwnerService ownerService;
 
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"","index","index.html"})
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
+        return "owners/index";
     }
 }
