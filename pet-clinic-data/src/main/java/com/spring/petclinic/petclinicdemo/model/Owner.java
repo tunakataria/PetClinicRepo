f7bb64e9db0e599b0a16ad7bac067ef7809adf5e
hiteshkataria <kataria.hitesh@yahoo.in>
2019-04-05 13:32:19 +0530

    adding owner contact properties. closes #18

--- pet-clinic-data/src/main/java/com/spring/petclinic/petclinicdemo/model/Owner.java
+++ pet-clinic-data/src/main/java/com/spring/petclinic/petclinicdemo/model/Owner.java
@@ -4,7 +4,27 @@ import java.util.Set;
 
 public class Owner extends Person{
 
+    String address;
+
+    String telephone;
+
     Set<Pet> pets ;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
