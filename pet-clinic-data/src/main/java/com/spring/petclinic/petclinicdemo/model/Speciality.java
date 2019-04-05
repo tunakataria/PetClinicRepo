c7d37ef1a31e219c53a1e21a9c610fc7d450f003
hiteshkataria <kataria.hitesh@yahoo.in>
2019-04-05 13:23:17 +0530

    Create Speciality and associated it to the vet. Closes #17

---
+++ pet-clinic-data/src/main/java/com/spring/petclinic/petclinicdemo/model/Speciality.java
@@ -0,0 +1,15 @@
+package com.spring.petclinic.petclinicdemo.model;
+
+public class Speciality extends  BaseEntity {
+
+    private String speciality;
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
+
