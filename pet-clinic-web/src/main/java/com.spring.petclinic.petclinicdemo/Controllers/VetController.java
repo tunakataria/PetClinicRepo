82f733a884cc249ec7f2cd9106e46654a8ff94ee
hiteshkataria <kataria.hitesh@yahoo.in>
2019-04-05 12:44:39 +0530

    Implementing master layout to vets page Closes #15

--- pet-clinic-web/src/main/java/com.spring.petclinic.petclinicdemo/Controllers/VetController.java
+++ pet-clinic-web/src/main/java/com.spring.petclinic.petclinicdemo/Controllers/VetController.java
@@ -1,6 +1,7 @@
 package com.spring.petclinic.petclinicdemo.Controllers;
 
 import com.spring.petclinic.petclinicdemo.service.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,13 @@ public class VetController {
 
     private final VetService vetService;
 
+    @Autowired
     public VetController(VetService vetService) {
         this.vetService = vetService;
     }
 
     @RequestMapping({"/vets","/vets.html"})
     public String listVets(Model model){
-
         model.addAttribute("vets", vetService.findAll() );
         return "vets/index";
     }
