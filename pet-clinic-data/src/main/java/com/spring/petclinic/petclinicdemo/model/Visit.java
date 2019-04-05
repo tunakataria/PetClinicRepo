3b6e57e546a2b14989422293a38b61fe10d5579d
hiteshkataria <kataria.hitesh@yahoo.in>
2019-04-05 13:14:01 +0530

    Create Visit, associated pets with owner and add a pet type #16

---
+++ pet-clinic-data/src/main/java/com/spring/petclinic/petclinicdemo/model/Visit.java
@@ -0,0 +1,36 @@
+package com.spring.petclinic.petclinicdemo.model;
+
+import java.time.LocalDate;
+
+public class Visit {
+
+    LocalDate date;
+
+    String description;
+
+    Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
